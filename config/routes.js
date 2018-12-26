5bdfa942906b6200fe6b330b074b4355f9e155fd
Aria Samandi <ariasamandi@gmail.com>
2018-12-26 12:28:20 -0800

    commit

--- config/routes.js
+++ config/routes.js
@@ -2,5 +2,8 @@ const Controller = require('./../controllers/controller');
 const path = require('path');
 module.exports = (app)=>{
     app.get('/api/index', Controller.index);
+    app.all("*", (req,res,next) => {
+        res.sendFile(path.resolve("./public/dist/public/index.html"))
+      });
     
 }
